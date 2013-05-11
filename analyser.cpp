40cc6cc1921601170eafe102d2e0e732cdba4d79
Jinglei Ren <jinglei.ren@gmail.com>
2013-05-11 11:29:33 +0800

    Add hasing to analyser.

--- analyser.cpp
+++ analyser.cpp
@@ -9,8 +9,22 @@
 #include <cstdio>
 #include <fstream>
 #include <stdint.h>
+#include <openssl/md5.h>
+
+#define DEDUP_LEN 1024 // bytes
 
 using namespace std;
+
+void handle_data(const unsigned char *data, const uint64_t len) {
+  unsigned char digest[MD5_DIGEST_LENGTH];
+  uint64_t pos = 0;
+  uint64_t dedup_len;
+
+  while (pos < len) {
+    dedup_len = pos + DEDUP_LEN > len ? len - pos : DEDUP_LEN;
+    MD5(data + pos, dedup_len, digest);
+  }
+}
 
 int main(int argc, char *argv[]) {
   if (argc < 2) {
@@ -20,14 +34,15 @@ int main(int argc, char *argv[]) {
   char *file_name = argv[1];
 
   fstream log_file(file_name, ios::in | ios::binary);
-  uint64_t len;
+  uint64_t seg_len;
   char *data = NULL;
-
-  log_file.read((char *)&len, sizeof(len));
+  
+  log_file.read((char *)&seg_len, sizeof(seg_len));
   while (log_file) {
-    data = (char *)realloc(data, len);
-    log_file.read(data, len);
-    log_file.read((char *)&len, sizeof(len));
+    data = (char *)realloc(data, seg_len);
+    log_file.read(data, seg_len);
+    handle_data((unsigned char *)data, seg_len);
+    log_file.read((char *)&seg_len, sizeof(seg_len));
   }
 
   free(data);
